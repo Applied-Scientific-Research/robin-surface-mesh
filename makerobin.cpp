6b61d47d064650496ae514c75e9b25c25a72fb16
Blake Hillier <blakehillier@mac.com>
2020-06-16 16:38:01 -0700

    deals with complex numbers

--- makerobin.cpp
+++ makerobin.cpp
@@ -6,6 +6,7 @@
 
 #include <cstdlib>
 #include <cmath>
+#include <complex>
 #include <iostream>
 #include <vector>
 #include <array>
@@ -20,11 +21,11 @@ int getsection (const double x) {
   return idx;
 }
 
-using SupEll = std::array<double,8>;
+using SupEll = std::array<std::complex<double>, 8>;
 
-double getsuperval (const double x, const SupEll& c) {
+std::complex<double> getsuperval (const double x, const SupEll& c) {
   //std::cout << "se is " << c[0] << " " << c[1] << " " << c[2] << " " << c[3] << " " << c[4] << " " << c[5] << " " << c[6] << " " << c[7] << std::endl;
-  std::cout << "comps are " << ((x+c[2])/c[3]) << " " << c[4] << std::endl;
+  //std::cout << "comps are " << ((x+c[2])/c[3]) << " " << c[4] << std::endl;
   return c[5] + c[6]*std::pow(c[0]+c[1]*std::pow((x-c[2])/c[3], c[4]), 1./c[7]);	// nan
   //return c[5] + c[6]*std::pow(c[0]+c[1]*std::pow((x+c[2])/c[3], c[4]), 1./c[7]);	// nan
   //return c[5] + c[6]*std::pow(c[0]+c[1]*std::pow((x+c[2])/c[3], c[4]), 1./2.0);	// nan
@@ -52,34 +53,34 @@ int main(int argc, char const *argv[]) {
                                  {5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0} };
 
   //double tx[3] = {0.5, 0.5, 0.5};
-  const size_t nx = 100;
-  const size_t nt = 80;
+  const size_t nx = 25;
+  const size_t nt = 20;
 
   std::cout << std::endl << "generating nodes" << std::endl;
   for (size_t ix=1; ix<=nx ; ++ix) {
     const double xol = 2.0 * ix / (double)nx;
     const int isec = 0;//getsection(xol);
     // compute H, W, Z0, and N from xol and the constants
-    const double H  = getsuperval(xol, hcoeff[isec]);
-    const double W  = getsuperval(xol, wcoeff[isec]);
-    const double Z0 = getsuperval(xol, zcoeff[isec]);
-    const double N  = getsuperval(xol, ncoeff[isec]);
-    std::cout << "at xol=" << xol << " have " << H << " " << W << " " << Z0 << " " << N << std::endl;
+    const std::complex<double> H  = getsuperval(xol, hcoeff[isec]);
+    const std::complex<double> W  = getsuperval(xol, wcoeff[isec]);
+    const std::complex<double> Z0 = getsuperval(xol, zcoeff[isec]);
+    const std::complex<double> N  = getsuperval(xol, ncoeff[isec]);
+    // std::cout << "at xol=" << xol << " have " << H << " " << W << " " << Z0 << " " << N << std::endl;
     for (size_t it=0; it<nt ; ++it) {
       const double theta = 2.0*3.14159265358979*it/(double)nt;
       // compute r from H, W, N, theta
-      const double r = std::pow( std::pow(0.25*H*W, N) /
+      const std::complex<double> r = std::pow( std::pow(0.25*H*W, N) /
                                 (std::pow(0.5*H*std::sin(theta), N)+
                                  std::pow(0.5*W*std::cos(theta), N))
                                 , 1.0/N);
       // compute yol, zol from r, theta, Z0
-      const double yol = r * std::sin(theta);
-      const double zol = r * std::cos(theta) + Z0;
-      std::cout << "  " << xol << " " << yol << " " << zol << std::endl;
-      exit(0);
+      const std::complex<double> yol = r * std::sin(theta);
+      const std::complex<double> zol = r * std::cos(theta) + Z0;
+      std::cout << "  " << xol << " " << yol.real() << " " << zol.real() << std::endl;
+      //exit(0);
     }
 
-    exit(0);
+    //exit(0);
     // make the triangles for this band
   }
 
